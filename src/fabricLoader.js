b3bf75be37a54fe6631e063a2f72129e967d5df6
Kritika M <kritikam1526@gmail.com>
2025-10-26 20:07:29 +0530

    Force update fabricLoader.js

--- src/fabricLoader.js
+++ src/fabricLoader.js
@@ -2,6 +2,6 @@
 // Ensures fabric is attached to window.fabric for Vite / modern bundlers.
 // Place this file in src/ and import it from Whiteboard.jsx.
 
-import fabric from "fabric";
-window.fabric = fabric;
-export default fabric;
+import * as fabric from "fabric"; 
+ window.fabric = fabric; 
+  export default fabric;
