f34613e215648ebdd10a3b6a46e508c27a37e7fb
Kritika M <kritikam1526@gmail.com>
2025-10-26 19:24:18 +0530

    Initial commit for Kryvo Frontend

--- src/fabricLoader.js
+++ src/fabricLoader.js
@@ -2,6 +2,6 @@
 // Ensures fabric is attached to window.fabric for Vite / modern bundlers.
 // Place this file in src/ and import it from Whiteboard.jsx.
 
-import * as fabric from "fabric";
+import fabric from "fabric";
 window.fabric = fabric;
 export default fabric;
